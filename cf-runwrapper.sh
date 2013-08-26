7e8607f458eb0f5421740267b082081720f77077
jonhenrik13 <jon.henrik.bjornstad@cfengine.com>
2013-08-26 15:32:23 +0200

    Added file header

--- cf-runwrapper.sh
+++ cf-runwrapper.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+##############################################################################
+#
+#   cf-runwrapper.sh
+#
+#   Copyright (C) cfengineers.net
+#
+#   Written and maintained by Jon Henrik Bjornstad <jonhenrik@cfengineers.net>
+#
+#   This program is free software; you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by the
+#   Free Software Foundation; version 3.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+##############################################################################
+
 # CONTROLWORD is the first bundle in the bundlesequence. It should be on the following format
 # [PROMISES|UPDATE|FAILSAFE]_[QUIET|VERBOSE|INFORM]_[DRYRUN|NODRYRUN]_[NOLOCK|LOCK]
 # eg.
